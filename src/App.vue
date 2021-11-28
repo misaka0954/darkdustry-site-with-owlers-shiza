23d4049c7e6ff44705416364dcf1874d3e83591a
Misaka <63801085+misaka0954@users.noreply.github.com>
2021-11-28 23:13:16 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <body>
+    <div id="app">
+      <HeadMenuBar />
+      <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+      <HelloWorld msg="Hello Hentai-chan!" />
+    </div>
+  </body>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import HeadMenuBar from "./components/HeadMenuBar";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    HeadMenuBar,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #AAFFAA;
+  margin-top: 0px;
+}
+body {
+  background-image: url("https://media.discordapp.net/attachments/810758723814948864/914600477453332600/back.png"); /*You will specify your image path here.*/
+  -moz-background-size: cover;
+  -webkit-background-size: cover;
+  background-size: cover;
+  background-position: top center !important;
+  background-repeat: no-repeat !important;
+  background-attachment: fixed;
+}
+</style>
---
+++ src/components/HeadMenuBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="menubar">
+    <ul>
+      <li>О сервере</li>
+      <li>Правила</li>
+      <li>Discord</li>
+      <li>Рейтинг Hexed</li>
+      <li>Глобальный рейтинг</li>
+      <li>Написать тому кто это делал</li>
+      <li>Хентай</li>
+    </ul>
+  </div>
+</template>
+<style>
+#menubar {
+  width: 95%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 0%;
+  height: auto;
+}
+
+ul {
+  text-transform: uppercase;
+  text-align: left;
+  display: inline;
+  margin: 0;
+  padding: 15px 4px 17px 0;
+  list-style: none;
+  -webkit-box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
+  -moz-box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
+}
+ul li {
+  font: bold 12px/18px sans-serif;
+  display: inline-block;
+  margin-right: -5px;
+  position: relative;
+  padding: 15px 20px;
+  background: #0000aa15;
+  color: #00FF00;
+  cursor: pointer;
+  -webkit-transition: all 0.2s;
+  -moz-transition: all 0.2s;
+  -ms-transition: all 0.2s;
+  -o-transition: all 0.2s;
+  transition: all 0.2s;
+}
+ul li:hover {
+  background: #00A000;
+  color: #f7f7f7;
+}
+</style>
